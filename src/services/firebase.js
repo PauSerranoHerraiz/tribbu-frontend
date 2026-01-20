9391ce982782da321425939824782084133f9894
Pau Serrano <pauserrano14@gmail.com>
2026-01-20 11:44:59 +0100

    feat: configure Firebase with environment variables and update .gitignore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDChqzfln2x4LJ199GGPG0YbgKThvf-xEw",
-    authDomain: "tribbu-22519.firebaseapp.com",
-    projectId: "tribbu-22519",
-    storageBucket: "tribbu-22519.firebasestorage.app",
-    messagingSenderId: "182061766748",
-    appId: "1:182061766748:web:964ad85455fd860db7ee99",
-    measurementId: "G-CVHZL7P93S"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializa Firebase
